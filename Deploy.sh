1511d714eb2b6d831289a675c9dbf70a48e6b349
vijaymanjunathas5 <110052997+vijaymanjunathas5@users.noreply.github.com>
2022-08-15 16:12:03 +0530

    Update Deploy.sh

--- Deploy.sh
+++ Deploy.sh
@@ -9,3 +9,4 @@ The new line added after git rest in uat
 another line added to test boh file on commit in uat
 adding new line to test fetch from master 
 adding to test single commint cherry-pin from maser
+nes lint to test ferch from mater
